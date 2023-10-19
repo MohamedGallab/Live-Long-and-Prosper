3cb4c7e9edfa8f50bb2a9777d09eec4b5a3ffab8
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2023-10-19 14:29:13 +0300

    we started yay

---
+++ src/LLAPSearch.java
@@ -0,0 +1,18 @@
+public class LLAPSearch {
+    // initialP rosperity;
+    // initialF ood, initialM aterials, initialEnergy;
+    // unitP riceF ood, unitP riceM aterials, unitP riceEnergy;
+    // amountRequestF ood, delayRequestF ood;
+    // amountRequestM aterials, delayRequestM aterials;
+    // amountRequestEnergy, delayRequestEnergy;
+    // priceBUILD1, foodUseBUILD1,
+    // materialsUseBUILD1, energyUseBUILD1, prosperityBUILD1;
+    // priceBUILD2, foodUseBUILD2,
+    // materialsUseBUILD2, energyUseBUILD2, prosperityBUILD1;
+    void solve(String intialState, String strategy, boolean visualize) {
+
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,7 @@
+public class Node {
+    int prosperity;
+    int food;
+    int materials;
+    int energy;
+    int moneySpent;
+}
