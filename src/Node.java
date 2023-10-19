a2b67a29f2a0d506e0648ee6e6816a110837ebb9
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2023-10-19 14:44:03 +0300

    wip

--- src/Node.java
+++ src/Node.java
@@ -3,5 +3,11 @@ public class Node {
     int food;
     int materials;
     int energy;
-    int moneySpent;
+    int moneySpent = 0;
+    Node(int prosperity, int food, int materials, int energy) {
+        this.prosperity = prosperity;
+        this.food = food;
+        this.materials = materials;
+        this.energy = energy;
+    }
 }
